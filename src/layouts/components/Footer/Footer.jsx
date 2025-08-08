84e1785c9ffe6f27005001bf88af7a8579e08731
nguyenquochuy174 <huymonkey6403@gmail.com>
2025-08-08 23:55:49 +0700

    push code component header footer

--- src/layouts/components/Footer/Footer.jsx
+++ src/layouts/components/Footer/Footer.jsx
@@ -1,7 +1,104 @@
+import classNames from 'classnames/bind';
+import styles from './Footer.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+    faAngleUp,
+    faEnvelope,
+    faLocationDot,
+    faPhone,
+} from '@fortawesome/free-solid-svg-icons';
+import {
+    faFacebook,
+    faFacebookMessenger,
+    faSquareInstagram,
+} from '@fortawesome/free-brands-svg-icons';
+const cx = classNames.bind(styles);
 function Footer() {
     return (
         <>
-            <h1>Footer</h1>
+            <div className={cx('footer')}>
+                <div className={cx('backToTop')}>
+                    <p>Quay lại đầu trang</p>
+                    <FontAwesomeIcon
+                        icon={faAngleUp}
+                        className={cx('iconAngleUp')}
+                    />
+                </div>
+                <div className={cx('main')}>
+                    <div className={cx('footerInfo')}>
+                        <ul>
+                            <li>
+                                <a href="#">
+                                    <FontAwesomeIcon icon={faLocationDot} />
+                                    <p>Trường Đại học Khoa học Huế</p>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <FontAwesomeIcon icon={faEnvelope} />
+                                    <p>trungmart75@gmail.com</p>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <FontAwesomeIcon icon={faPhone} />
+                                    <p>0367376403</p>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className={cx('footerMenu')}>
+                        <ul>
+                            <li>
+                                <a href="/UserHome">Trang Chủ</a>
+                            </li>
+                            <li>
+                                <a href="/UserProduct">Sản Phẩm</a>
+                            </li>
+                            <li>
+                                <a href="/UserIntroduce">Giới Thiệu</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className={cx('footerRequire')}>
+                        <ul>
+                            <li>
+                                <a href="#">Chính sách giao hàng</a>
+                            </li>
+                            <li>
+                                <a href="#">Điều khoản và điều kiện sử dụng</a>
+                            </li>
+                            <li>
+                                <a href="#">Chính sách bảo mật</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className={cx('footerSocial')}>
+                        <ul>
+                            <li>
+                                <a href="#">
+                                    <FontAwesomeIcon icon={faSquareInstagram} />
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <FontAwesomeIcon icon={faFacebook} />
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <FontAwesomeIcon
+                                        icon={faFacebookMessenger}
+                                    />
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div className={cx('endFooter')}>
+                    <p>© 2025 - Bản quyền thuộc về TrungMart</p>
+                </div>
+            </div>
         </>
     );
 }
--- src/layouts/components/User/Header/Header.jsx
+++ src/layouts/components/User/Header/Header.jsx
@@ -1,9 +1,84 @@
+import classNames from 'classnames/bind';
 import { MenuUser } from '~/components/MenuUser/MenuUser';
+import styles from './Header.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+    faFacebook,
+    faFacebookMessenger,
+} from '@fortawesome/free-brands-svg-icons';
+import {
+    faBell,
+    faCartShopping,
+    faSearch,
+    faUser,
+} from '@fortawesome/free-solid-svg-icons';
+import images from '~/assets/images/image';
+
+const cx = classNames.bind(styles);
 
 function Header() {
     return (
         <>
-            <h2>Tìm kiếm</h2>
+            <div className={cx('container')}>
+                <div className={cx('headerTop')}>
+                    <div className={cx('infoHeader')}>
+                        <FontAwesomeIcon
+                            icon={faFacebook}
+                            className={cx('iconFace')}
+                        />
+                        <p>
+                            For any queries - please contact customer
+                            care-0367376403
+                        </p>
+                    </div>
+                </div>
+
+                <div className={cx('headerBottom')}>
+                    <div className={cx('headerContent')}>
+                        <div className={cx('logo')}>
+                            <img src={images.logo} alt="TrungMart" />
+                        </div>
+                        <div className={cx('formSearch')}>
+                            <input
+                                type="text"
+                                placeholder="Nhập vào đây để tìm kiếm . . ."
+                            />
+                            <FontAwesomeIcon
+                                icon={faSearch}
+                                className={cx('iconSearch')}
+                            />
+                        </div>
+                        <div className={cx('menuIconHeader')}>
+                            <ul>
+                                <li>
+                                    <a href="/UserNotification">
+                                        <FontAwesomeIcon icon={faBell} />
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="/UserMessage">
+                                        <FontAwesomeIcon
+                                            icon={faFacebookMessenger}
+                                        />
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="/UserShoppingCart">
+                                        <FontAwesomeIcon
+                                            icon={faCartShopping}
+                                        />
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="/UserProfileInfo">
+                                        <FontAwesomeIcon icon={faUser} />
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
             <MenuUser />
         </>
     );
